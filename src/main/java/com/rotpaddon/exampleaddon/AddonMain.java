7bba4153580e6fe6189ea4799d1cc06405e587a2
filipe <filipecraft00@gmail.com>
2024-11-29 03:28:03 -0300

    im dumb

---
+++ src/main/java/com/rotpaddon/exampleaddon/AddonMain.java
@@ -0,0 +1,34 @@
+package com.rotpaddon.exampleaddon;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.rotpaddon.exampleaddon.init.InitEntities;
+import com.rotpaddon.exampleaddon.init.InitSounds;
+import com.rotpaddon.exampleaddon.init.InitStands;
+import com.rotpaddon.exampleaddon.action.CalamityAbility;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(AddonMain.MOD_ID)
+public class AddonMain {
+    public static final String MOD_ID = "rotp-wou";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public AddonMain() {
+        // Registra os eventos no Forge EventBus
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Corrigir a linha aqui para registrar no Forge EventBus
+        MinecraftForge.EVENT_BUS.addListener(CalamityAbility::onEntityAttacks);
+
+        // Registra outras entidades e sons
+        InitEntities.ENTITIES.register(modEventBus);
+        InitSounds.SOUNDS.register(modEventBus);
+        InitStands.ACTIONS.register(modEventBus);
+        InitStands.STANDS.register(modEventBus);
+    }
+}
