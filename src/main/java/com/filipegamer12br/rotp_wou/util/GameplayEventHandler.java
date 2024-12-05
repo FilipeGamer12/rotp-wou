41395c4a646f298e8d71e30857a20945b13694c6
filipe <filipecraft00@gmail.com>
2024-12-05 17:11:40 -0300

    changed somethings

--- src/main/java/com/filipegamer12br/rotp_wou/util/GameplayEventHandler.java
+++ src/main/java/com/filipegamer12br/rotp_wou/util/GameplayEventHandler.java
@@ -2,20 +2,20 @@ package com.filipegamer12br.rotp_wou.util;
 
 import com.filipegamer12br.rotp_wou.WonderOfYouAddon;
 import com.filipegamer12br.rotp_wou.entity.WonderOfYouEntity;
-import com.filipegamer12br.rotp_wou.init.InitSounds;
 import com.filipegamer12br.rotp_wou.init.InitStands;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = WonderOfYouAddon.MOD_ID)
 public class GameplayEventHandler {
     @SubscribeEvent
-    public static void onHurtEvent(LivingHurtEvent event){
+    public static void onHurtEvent(LivingHurtEvent event) {
         LivingEntity livingEntity = event.getEntityLiving();
         if (livingEntity != null && livingEntity.isAlive() && !livingEntity.level.isClientSide()) {
             IStandPower.getStandPowerOptional(livingEntity).ifPresent(power -> {
@@ -23,11 +23,18 @@ public class GameplayEventHandler {
                     if (power.getStandManifestation() instanceof WonderOfYouEntity) {
                         WonderOfYouEntity wonderOfYouEntity = (WonderOfYouEntity) power.getStandManifestation();
                         if (wonderOfYouEntity.isCalamityEnabled()) {
+                            // Dano negado em 85% (recebe apenas 15% do dano)
                             float damageToOurWOU = event.getAmount();
+                            event.setAmount(damageToOurWOU * 0.15F); // 15% do dano original
+
                             if (event.getSource().getEntity() instanceof LivingEntity) {
                                 LivingEntity attacker = (LivingEntity) event.getSource().getEntity();
-                                attacker.hurt(event.getSource(), damageToOurWOU); // 100: duration (in ticks) | 1: amplifier (levels, from 0 to 255) | idk | idk | showIcon
-                                attacker.addEffect(new EffectInstance(Effects.POISON, 100, 1, false, false, true));
+                                // Reflete o dano de volta ao atacante
+                                attacker.hurt(event.getSource(), damageToOurWOU); // Reflete o dano original
+                                // Aplica efeitos negativos ao atacante
+                                attacker.addEffect(new EffectInstance(Effects.POISON, 100, 2, false, false, true)); // Veneno II.
+                                attacker.addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN, 100, 2, false, false, true)); // Lentidão II.
+                                attacker.addEffect(new EffectInstance(Effects.BLINDNESS, 60, 0, false, false, true)); // Cegueira.
                             }
                         }
                     }
