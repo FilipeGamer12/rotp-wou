8f0d4c7a318ffd9e8e9109ab9bb7bc643343206f
filipe <filipecraft00@gmail.com>
2024-12-11 21:39:39 -0300

    changed some things

--- src/main/java/com/filipegamer12br/rotp_wou/client/render/WonderOfYouModel.java
+++ src/main/java/com/filipegamer12br/rotp_wou/client/render/WonderOfYouModel.java
@@ -12,7 +12,6 @@ import com.filipegamer12br.rotp_wou.entity.WonderOfYouEntity;
 //import net.minecraft.client.renderer.model.ModelRenderer;
 
 public class WonderOfYouModel extends HumanoidStandModel<WonderOfYouEntity> {
-    //private ModelRenderer pickaxe;
 
     public WonderOfYouModel() {
         super();
@@ -28,30 +27,6 @@ public class WonderOfYouModel extends HumanoidStandModel<WonderOfYouEntity> {
         };
     }
 
-    @Override
-    protected void initActionPoses() { // TODO pickaxe throwing anim
-        actionAnim.put(StandPose.RANGED_ATTACK, new PosedActionAnimation.Builder<WonderOfYouEntity>()
-                .addPose(StandEntityAction.Phase.BUTTON_HOLD, new ModelPose<>(new RotationAngle[] {
-                        new RotationAngle(body, 0.0F, -0.48F, 0.0F),
-                        new RotationAngle(leftArm, 0.0F, 0.0F, -0.7854F),
-                        new RotationAngle(leftForeArm, 0.0F, 0.0F, 0.6109F),
-                        new RotationAngle(rightArm, -1.0908F, 0.0F, 1.5708F),
-                        new RotationAngle(rightForeArm, 0.0F, 0.0F, 0.0F)
-                }))
-                .build(idlePose));
-
-        super.initActionPoses();
-    }
-
-    /*
-    @Override
-    public void prepareMobModel(WonderOfYouEntity entity, float walkAnimPos, float walkAnimSpeed, float partialTick) {
-        super.prepareMobModel(entity, walkAnimPos, walkAnimSpeed, partialTick);
-        if (pickaxe != null) {
-            pickaxe.visible = entity.isCalamityActiveEnabled();
-        }
-    }
-*/
     @Override // TODO idle pose
     protected ModelPose<WonderOfYouEntity> initIdlePose() {
         return super.initIdlePose();
